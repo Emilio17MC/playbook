8af8ae22653d672cafb26e026289aa811e252c89
Emilio <emilio17mc@gmail.com>
2022-04-21 02:13:13 -0500

    Paso 5: Express API - POST: Crea un endpoint que se encargue de crear un explorer

--- weekly_mission_3/exercises/express_api/app.js
+++ weekly_mission_3/exercises/express_api/app.js
@@ -27,6 +27,13 @@ app.get("/v1/explorers/:id", (req, res) => {
   res.status(200).json(explorer);
 });
 
+app.post("/v1/explorers", (req, res) => {
+  console.log(`API Explorers POST requests ${new Date()}`);
+
+  const requestBody = req.body; // Parámetros de un cliente
+  res.status(201).json({ message: "Created" });
+});
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
